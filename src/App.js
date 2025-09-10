f7b97cda25b310db7c128663badca4b8509599c1
Mahdia-123 <160815889+Mahdia-123@users.noreply.github.com>
2025-09-10 17:28:04 +0430

    inatial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import AiTranslator from "./AiTranslator";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <AiTranslator />
+    </div>
+  );
+}
+
+export default App;
